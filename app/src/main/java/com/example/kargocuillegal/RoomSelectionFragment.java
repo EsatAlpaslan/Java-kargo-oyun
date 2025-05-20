8c08827b4b70058b43c6ab21343362934fcc5679
Seyma Sahin <seymasahhin23@gmail.com>
2025-05-20 20:25:44 +0300

    üç kapılı odadan oyun oynanan odaya geçiş eklendi

--- app/src/main/java/com/example/kargocuillegal/RoomSelectionFragment.java
+++ app/src/main/java/com/example/kargocuillegal/RoomSelectionFragment.java
@@ -3,24 +3,18 @@ package com.example.kargocuillegal;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RoomSelectionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RoomSelectionFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +22,6 @@ public class RoomSelectionFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RoomSelectionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RoomSelectionFragment newInstance(String param1, String param2) {
         RoomSelectionFragment fragment = new RoomSelectionFragment();
         Bundle args = new Bundle();
@@ -58,7 +43,14 @@ public class RoomSelectionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_room_selection, container, false);
+        View view = inflater.inflate(R.layout.fragment_room_selection, container, false);
+
+        // Sol kapıya etkileşim
+        Button btnLeftDoor = view.findViewById(R.id.btnLeftDoor);
+        btnLeftDoor.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_roomSelectionFragment_to_mainRoomFragment)
+        );
+
+        return view;
     }
 }
