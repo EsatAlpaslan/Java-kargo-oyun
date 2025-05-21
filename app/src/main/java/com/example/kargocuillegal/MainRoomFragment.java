ac015eefca2b15e74082aae9d61d1dabce4eb757
Esat Alpaslan <2003esat2003@gmail.com>
2025-05-21 18:54:25 +0300

    lamba eklendi

--- app/src/main/java/com/example/kargocuillegal/MainRoomFragment.java
+++ app/src/main/java/com/example/kargocuillegal/MainRoomFragment.java
@@ -1,26 +1,23 @@
 package com.example.kargocuillegal;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainRoomFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 public class MainRoomFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +25,6 @@ public class MainRoomFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainRoomFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainRoomFragment newInstance(String param1, String param2) {
         MainRoomFragment fragment = new MainRoomFragment();
         Bundle args = new Bundle();
@@ -58,7 +46,26 @@ public class MainRoomFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_room, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageView alarmLight = view.findViewById(R.id.alarmLight);
+
+        // 3 saniye sonra alarmı başlat
+        new Handler().postDelayed(() -> {
+            alarmLight.setVisibility(View.VISIBLE);
+
+            // ObjectAnimator ile daha güçlü yanıp sönme efekti
+            ObjectAnimator animator = ObjectAnimator.ofFloat(alarmLight, "alpha", 1f, 0.2f);
+            animator.setDuration(400); // yanıp sönme süresi
+            animator.setRepeatCount(ValueAnimator.INFINITE);
+            animator.setRepeatMode(ValueAnimator.REVERSE);
+            animator.start();
+
+        }, 3000); // 3 saniye gecikmeli başlat
     }
 }
